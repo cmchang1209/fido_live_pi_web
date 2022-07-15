dfe04cebf1ce3fbcf01de132a60201e03188e180
david <cmchang120956@gmail.com>
2022-07-15 03:22:28 +0000

    install axios && webrtc-streamer

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import globalFunction from './plugins/global'
+import "webrtc-streamer/html/libs/adapter.min"
+import "webrtc-streamer/html/webrtcstreamer"
 
 Vue.config.productionTip = false
+
 
 Vue.use(globalFunction)
 
